cf2cf0c9a1ec5e24f14f7892f39db7ebf6ea1817
PROHOR <ahprohor88@gmail.com>
2021-09-03 20:20:35 +0600

    functionality added.

--- js/book-website.js
+++ js/book-website.js
@@ -2,25 +2,49 @@
 const searchBook = () => {
 
     const searchField = document.getElementById('search-field');
-    const seacrchText = searchField.value;
+    const searchText = searchField.value;
 
-    const url =`https://openlibrary.org/search.json?q=${seacrchText}`
+    // clear search field 
+    searchField.value = '';
+
+    if(searchText === ''){
+        const searchResult = document.getElementById('error-message');
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <h1 class="text-center text-danger">Please write something</h1>
+        `
+        searchResult.appendChild(div);
+    }
+    else{
+        // load data 
+    const url =`https://openlibrary.org/search.json?q=${searchText}`
     fetch(url)
     .then(resposnse => resposnse.json())
     .then(data => displaySearchResult(data.docs));
-
-    searchField.value = '';
+    }
 }
 
 const displaySearchResult = books =>{
+    //console.log(books.length);
     const searchResult =  document.getElementById('search-result');
+    searchResult.innerHTML = '';
+    const url =`https://covers.openlibrary.org/b/id/{cover_i}-M.jpg`
+
+    // total book of search
+    const totalBook = document.getElementById('total-book');
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <h1 class="text-center">Total Book = ${books.length}</h1>
+    `
+    searchResult.appendChild(div);
+
     books.forEach(book =>{
         console.log(book);
         const div = document.createElement('div');
-        div.classList.add('col');
+        div.classList.add('col-md-3');
         div.innerHTML = `
         <div class="card h-100">
-                <img src="" class="card-img-top" alt="..." />
+                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top mx-auto" alt="..." />
             <div class="card-body">
                 <h5 class="card-title">${book.title}</h5>
                 <p class="card-text">
